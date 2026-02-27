a2b6da31f2b02013354db77a0340058e521c8fc0
navin3756 <86693520+navin3756@users.noreply.github.com>
2026-02-27 09:42:06 +0000

    Add Android project, Capacitor config, built dist

---
+++ android/app/src/main/java/com/drpal/fitnesspal/MainActivity.java
@@ -0,0 +1,5 @@
+package com.drpal.fitnesspal;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.drpal.fitnesspal',
+  appName: 'FitnessPAL',
+  webDir: 'dist'
+};
+
+export default config;
